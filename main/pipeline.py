3b62f0320f20563c8cf442284edaed2424e94d56
axelBaher <shiftmainsolo@gmail.com>
2023-06-09 17:16:05 +0300

    Some pipeline scratches.

--- main/pipeline.py
+++ main/pipeline.py
@@ -16,16 +16,20 @@ def generate_models():
     models = dict({
         "LeNet5": m.ModelLeNet5(),
         "AlexNet": m.ModelAlexNet(),
-        "VGGNetD": m.ModelVGGNetD(),
         "GoogLeNet": m.ModelGoogLeNet(),
-        "ResNet34": m.ModelResNet34()
+        "ResNet34": m.ModelResNet34(),
+        "VGGNetD": m.ModelVGGNetD()
     })
     return models
 
 
 def main():
-
-    prep.main()
+    epoch = list([1, 2, 3])
+    batch_size = list([16, 32, 64, 128, 256, 512])
+    models = generate_models()
+    for model in models:
+        # call train with epoch and batch_size parameters
+        pass
 
 
 if __name__ == "__main__":
